7e20e50c10b32ae4cd5b8b82b0c23164175d0907
jurasbjj <67524697+jurasbjj@users.noreply.github.com>
2021-02-09 21:49:20 +0100

    Add files via upload

--- js/landing.js
+++ js/landing.js
@@ -1,82 +1,81 @@
-$(window).load(function(){
-    setTimeout(function(){
-        $('.page-header img:nth-of-type(2)').addClass('active');
+$(window).load(function () {
+    setTimeout(function () {
+        $('.page-header-logo').addClass('active');
     }, 1000)
 })
 //desktop aktywacja nawigacji
- if(mediaQueryDesktop.matches){
-  $(window).scroll(function () {
-     if ($(window).scrollTop() > 64) {
-        $('nav').addClass('active');
-     } else {
-        $('nav').removeClass('active');
-     }
-  });
- }
+if (mediaQueryDesktop.matches) {
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 64) {
+            $('nav').addClass('active');
+        } else {
+            $('nav').removeClass('active');
+        }
+    });
+}
 
 // ###################################################### FAQ ######################################################
 const quests = document.querySelectorAll('.question')
 
- quests.forEach(function(quest){
-     quest.addEventListener('click', function(){
+quests.forEach(function (quest) {
+    quest.addEventListener('click', function () {
         //  console.log(this.dataset.id)
-         quests[this.dataset.id].classList.toggle('active');
-         for (i = 0; i < quests.length; i++){
-             if (i == this.dataset.id) continue;
-             quests[i].classList.remove('active');
-         }
-     })
- })
+        quests[this.dataset.id].classList.toggle('active');
+        for (i = 0; i < quests.length; i++) {
+            if (i == this.dataset.id) continue;
+            quests[i].classList.remove('active');
+        }
+    })
+})
 
 // ###################################################### KONIEC FAQ ######################################################
 
 // ###################################################### SLIDER ABOUT_US ######################################################
 // MOBILE 
-  var swiper = new Swiper('.swiper-container-gallery', {
+var swiper = new Swiper('.swiper-container-gallery', {
     speed: 800,
     effect: 'slide',
     slidesPerView: '1',
     loop: false,
     pagination: {
-       el: '.slide-number',
-      type: 'custom',
-      renderCustom: function (swiper, current, total) {
-      return ('0' + current).slice(-2) + ' / ' + ('0' + total).slice(-2);
-}
+        el: '.slide-number',
+        type: 'custom',
+        renderCustom: function (swiper, current, total) {
+            return ('0' + current).slice(-2) + ' / ' + ('0' + total).slice(-2);
+        }
     },
     navigation: {
-      nextEl: '.right',
-      prevEl: '.left',
+        nextEl: '.right',
+        prevEl: '.left',
     },
-     autoplay: {
-         delay: 4000,
-         disableOnInteraction: false,
-       },
-       breakpoints:{
-           801:{
-                slidesPerView: 3,
-                spaceBetween: 0,
-                loop: true,
-                centeredSlides: true,
+    autoplay: {
+        delay: 4000,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        801: {
+            slidesPerView: 3,
+            spaceBetween: 0,
+            loop: true,
+            centeredSlides: true,
 
-               autoplay:{
-                   delay: 6000,
-               },
+            autoplay: {
+                delay: 6000,
+            },
 
-           }
-       }
-  });
- // #about_us SLIDER WRAP BUTTON
- $('#about_us button').click(function(){
-     $('.wrap-text').toggleClass('active')
-     $('#about_us button').toggleClass('active');
-     if ($("#about_us button").hasClass("active")) {
-         $("#about_us button span").text('Czytaj mniej');
-       }
-     else {
-         $('#about_us button span').text('Czytaj więcej')
-     }
- })
+        }
+    }
+});
+// #about_us SLIDER WRAP BUTTON
+$('#about_us button').click(function () {
+    $('.wrap-text').toggleClass('active')
+    $('#about_us button').toggleClass('active');
+    if ($("#about_us button").hasClass("active")) {
+        $("#about_us button span").text('Czytaj mniej');
+    } else {
+        $('#about_us button span').text('Czytaj więcej')
+    }
+})
 
 // ###################################################### SLIDER ABOUT_US ######################################################
 
@@ -85,28 +84,15 @@ const quests = document.querySelectorAll('.question')
 
 
 //  ###################################################### MOBILE TEXT CONTENT ###################################################
-if (mediaQueryMobile.matches){
+if (mediaQueryMobile.matches) {
     h2[2].textContent = 'Masz jakiekolwiek pytania? Napisz do nas.'
     footerText[0].textContent = 'Copyright © Złomiarz Team Gdańsk'
     footerText[1].textContent = "";
 }
 
- if(mediaQueryDesktop.matches){
-    //  $(".swiper-slide#s1 img").attr("src","img/about_us/onas1.webp");
-    //  $(".swiper-slide#s2 img").attr("src","img/about_us/onas2.webp");
-    //  $(".swiper-slide#s3 img").attr("src","img/about_us/onas3.webp");
-    //  $(".swiper-slide#s4 img").attr("src","img/about_us/onas4.webp");
-    //  $(".swiper-slide#s5 img").attr("src","img/about_us/onas5.webp");
-    //  $(".swiper-slide#s6 img").attr("src","img/about_us/onas6.webp");
-
-     $(".bg-logo").attr("src", $(".bg-logo").data().src);
-     $(".card .desktop").attr("src", $(".card .desktop").data().src);
-     $(".break img").attr("src", $(".break img").data().src);
-     $(".page-header img:nth-of-type(1)").attr("src", $(".page-header img:nth-of-type(1)").data().src);
-     $(".page-header img:nth-of-type(2)").attr("src", $(".page-header img:nth-of-type(2)").data().src);
-    
-    
- }
+if (mediaQueryDesktop.matches) {
+    $(".bg-logo").attr("src", $(".bg-logo").data().src);
+}
 
 
 //  ################################################### KONIEC MOBILE TEXT CONTENT ###############################################
@@ -121,25 +107,25 @@ var swiper = new Swiper('.swiper-container-review', {
     spaceBetween: 20,
     pagination: {
         el: '#review .slide-number',
-       type: 'custom',
-       renderCustom: function (swiper, current, total) {
-       return ('0' + current).slice(-2) + ' / ' + ('0' + total).slice(-2);
- }
-     },
-     navigation: {
-       nextEl: '#review .right',
-       prevEl: '#review .left',
-     },
-     breakpoints:{
-        801:{
-             slidesPerView: 2,
-             spaceBetween: 20,
-             loop: false,
+        type: 'custom',
+        renderCustom: function (swiper, current, total) {
+            return ('0' + current).slice(-2) + ' / ' + ('0' + total).slice(-2);
+        }
+    },
+    navigation: {
+        nextEl: '#review .right',
+        prevEl: '#review .left',
+    },
+    breakpoints: {
+        801: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+            loop: false,
 
         }
     }
 });
 // fix mobile swiper 
-if(mediaQueryMobile.matches){swiper.snapGrid[swiper.snapGrid.length - 1] = swiper.slidesGrid[swiper.slidesGrid.length - 1];}
-
-
+if (mediaQueryMobile.matches) {
+    swiper.snapGrid[swiper.snapGrid.length - 1] = swiper.slidesGrid[swiper.slidesGrid.length - 1];
+}
